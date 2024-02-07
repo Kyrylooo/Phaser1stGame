1d889b9c52bae7a7560291494465b693fc93a7c8
Kyrylooo <146863438+Kyrylooo@users.noreply.github.com>
2024-02-07 10:46:39 +0200

    добавив картинки

--- game.js
+++ game.js
@@ -17,8 +17,21 @@ function preload ()
 
 function create ()
 {
+    this.add.image(400, 300, 'sky');
+    this.add.image(400, 300, 'star');
 }
 
 function update ()
 {
+}
+function preload () // Добавленя картинок
+{
+    this.load.image('sky', 'assets/sky.png');
+    this.load.image('ground', 'assets/platform.png'); 
+    this.load.image('star', 'assets/star.png');
+    this.load.image('bomb', 'assets/bomb.png');
+    this.load.spritesheet('dude', 
+        'assets/dude.png',
+        { frameWidth: 32, frameHeight: 48 }
+    );
 }
